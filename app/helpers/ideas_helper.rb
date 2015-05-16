26ab4e4116618a290194b9c95bc6e0bf2d9cf857
abeylann <alix_1995@live.com.mx>
2015-05-16 16:34:10 +0100

    Use picture_store on production

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,9 @@
 module IdeasHelper
+	def picture_image_tag(idea)
+		if Rails.env.production?
+			image_tag(idea.picture_store_url) if idea.picture_store.present?
+		else
+			image_tag(idea.picture_url) if idea.picture.present?
+		end
+	end
 end
